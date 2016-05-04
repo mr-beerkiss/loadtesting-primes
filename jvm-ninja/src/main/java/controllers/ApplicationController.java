2e28527d629d23edd11283f531da26598b90690e
Darren Beukes <darren.beukes@gmail.com>
2016-05-04 07:56:16 +0100

    Initial import.

---
+++ jvm-ninja/src/main/java/controllers/ApplicationController.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (C) 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers;
+
+import ninja.Result;
+import ninja.Results;
+
+import com.google.inject.Singleton;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Singleton
+public class ApplicationController {
+
+    protected long nextPrime = 1;
+    //protected HashSet<Long> primes = new HashSet<Long>(50);
+    protected Set<Long> primes = ConcurrentHashMap.newKeySet();
+    private static final int LOOP_LIMIT = 100000;
+    private static final long[] PRIME_DIVISORS = { 2L, 3L, 5L, 7L };
+
+
+    public int indexOf(long[] array, long value) {
+        int index = -1;
+
+        for ( int i=0; i < array.length; i++ ) {
+            if ( array[i] == value ) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    public Result index() {
+
+        int counter = 0;
+        boolean foundPrime = false;
+
+        while ( !foundPrime && counter < LOOP_LIMIT ) {
+            nextPrime += 1L;
+            counter += 1;
+            foundPrime = true;
+
+            if ( nextPrime != 1L && indexOf(PRIME_DIVISORS, nextPrime) == -1 ) {
+                for ( Long l : primes ) {
+                    if ( nextPrime % l == 0 ) {
+                        foundPrime = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if ( counter < LOOP_LIMIT ) {
+            primes.add(nextPrime);
+        }
+
+
+        Result result = Results.html();
+
+        //result.render("name", "Darren");
+        result.render("nextPrime", nextPrime);
+        result.render("primes", primes);
+
+
+        return result;
+
+    }
+    
+    public Result helloWorldJson() {
+        
+        SimplePojo simplePojo = new SimplePojo();
+        simplePojo.content = "Hello World! Hello Json!";
+
+        return Results.json().render(simplePojo);
+
+    }
+    
+    public static class SimplePojo {
+
+        public String content;
+        
+    }
+}
---
+++ node/app.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const cluster = require('cluster');
+
+if ( cluster.isMaster ) {
+    
+    // create a worker proocess for each CPU
+    let workers = 0;
+    const cpuCount = require('os').cpus().forEach(next => {
+        cluster.fork();
+        workers += 1;
+    });
+    
+    console.log(`Workers created: ${workers}`);
+    
+} else {
+    const express = require('express');
+    const nunjucks = require('nunjucks');
+
+    const app = express();
+    // express error handling middleware: http://expressjs.com/en/guide/error-handling.html
+    app.use((err, req, res, next) => {
+        console.log(err.stack);
+        res.status(500).send('Something broke!');
+    });
+    
+    const primeDivisors = [2, 3, 5, 7];
+
+    let nextPrime = 1;
+    let primes = new Set();
+
+ 
+    nunjucks.configure('views', {
+        autoescape: true,
+        express: app
+    });
+
+
+
+    app.get('/', (req, res) => {
+        
+        // console.log('get main route');    
+        const limit = 100000;
+        let counter = 0;
+        let foundPrime = false;
+        
+        while ( !foundPrime && counter < limit ) {
+            nextPrime += 1;
+            counter += 1;
+            foundPrime = true;
+            if ( nextPrime !== 1 && primeDivisors.indexOf(nextPrime) === -1 ) {        
+                for ( let item of primes ) {
+                    if ( nextPrime % item === 0 ) {
+                        foundPrime = false;
+                        break;
+                    }
+                }  
+            }
+        }
+        
+        
+        
+        let counterExceeded = counter >= limit;
+        if ( !counterExceeded ) {
+            //console.log(`next prime found was ${nextPrime} by worker ${cluster.worker.id}`);
+            primes.add(nextPrime);
+            res.render('index.html', { primes: [...primes] });
+        } else {
+            //console.log('counter exceeded');
+            res.status(201).render('index.html');
+        }
+        
+    
+    
+    });
+
+    app.listen(3001, () => {
+        console.log(`Worker ${cluster.worker.id} started on port 3001, with error handling and graceful exit!`);
+    });
+    
+    process.on('uncaughtxception', err => {
+        console.log('Uncaught exception: ', err);
+    });
+    
+    process.on('exit', () => {
+        console.log(`Worker ${cluster.worker.id} exiting, highest prime: ${nextPrime}`);
+    });
+    process.on('SIGINT', () => {
+        console.log(`Worker ${cluster.worker.id} received SIGINT, highest prime: ${nextPrime}`);
+    });
+}
+
